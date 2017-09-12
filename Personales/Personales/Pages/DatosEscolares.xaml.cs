27338101eb0835940f8c70daee74f07fba20635e
Eduardo Campos <iwame@DESKTOP-UM7N6BP>
2017-09-11 22:27:29 -0500

    Add project files.

---
+++ Personales/Personales/Pages/DatosEscolares.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Personales.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DatosEscolares : ContentPage
+    {
+        public DatosEscolares()
+        {
+            InitializeComponent();
+        }
+        public void nextPage(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new DatosSociales());
+        }
+    }
+}
---
+++ Personales/Personales/Pages/DatosSociales.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Personales.Pages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class DatosSociales : ContentPage
+	{
+		public DatosSociales ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
